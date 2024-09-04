e38c022ca3f3191a6f56ae1979dea103f7f306c8
root <root@ip-172-31-16-118.ec2.internal>
2024-09-04 12:26:17 +0000

    infra code is added

---
+++ providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.59.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.my-access_key
+  secret_key = var.my-secret_key
+}
+
